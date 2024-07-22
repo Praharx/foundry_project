7fb7a5c550e9f2565c96d4bafd4d7f3854083ff6
harshana15 <harshana555prajapati@gmail.com>
2024-07-22 15:14:13 +0530

    sort of tried zksync

--- script/DeploySimpleToken.s.sol
+++ script/DeploySimpleToken.s.sol
@@ -4,13 +4,11 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {SimpleToken} from "../src/SimpleToken.sol";
 
-contract DeploySimpleToken is Script{
-    function run() external returns (SimpleToken){
+contract DeploySimpleToken is Script {
+    function run() external returns (SimpleToken) {
         vm.startBroadcast();
         SimpleToken simpleToken = new SimpleToken(200);
         vm.stopBroadcast();
         return simpleToken;
     }
 }
-
-
--- src/SimpleToken.sol
+++ src/SimpleToken.sol
@@ -6,7 +6,7 @@ contract SimpleToken {
     string public symbol = "STK";
     uint8 public decimals = 18;
     uint256 public totalSupply;
-    
+
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
@@ -22,17 +22,17 @@ contract SimpleToken {
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(_to != address(0), "Invalid address");
         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-        
+
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
-        
+
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
-        
+
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
@@ -41,11 +41,11 @@ contract SimpleToken {
         require(_to != address(0), "Invalid address");
         require(balanceOf[_from] >= _value, "Insufficient balance");
         require(allowance[_from][msg.sender] >= _value, "Allowance exceeded");
-        
+
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
         allowance[_from][msg.sender] -= _value;
-        
+
         emit Transfer(_from, _to, _value);
         return true;
     }
